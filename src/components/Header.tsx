f611f3246ccc95e9957f323074e7aaf041a4b43e
Brandon Franks <bgfranks.dev@gmail.com>
2022-11-30 12:31:52 -0500

    finished the base styling of the navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,6 +33,7 @@ export default function Header() {
         <p className='flex-1 hidden ml-2 lg:inline'>Home</p>
         <ChevronDownIcon className='h-5 w-5' />
       </div>
+      {/* search */}
       <form className='flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1'>
         <MagnifyingGlassIcon className='h-6 w-6 text-gray-400' />
         <input
@@ -42,15 +43,31 @@ export default function Header() {
         />
         <button hidden type='submit' />
       </form>
-      <div className='flex'>
+      {/* nav buttons */}
+      <div className='mx-5 text-gray-500 space-x-2 items-center hidden lg:flex'>
         <SparklesIcon className='icon' />
         <GlobeAsiaAustraliaIcon className='icon' />
-        <VideoCameraIcon />
+        <VideoCameraIcon className='icon' />
         <hr className='h-10 border border-gray-100' />
         <ChatBubbleOvalLeftEllipsisIcon className='icon' />
         <BellIcon className='icon' />
         <PlusIcon className='icon' />
         <MegaphoneIcon className='icon' />
+      </div>
+      <div className='ml-5 flex items-center lg:hidden'>
+        <Bars3Icon className='icon' />
+      </div>
+      {/* Sign in/out */}
+      <div className='hidden lg:flex items-center space-x-2 border border-gray-100 cursor-pointer p-2'>
+        <div className='relative h-5 w-5 flex-shrink-0'>
+          <Image
+            src='https://links.papareact.com/23l'
+            objectFit='contain'
+            layout='fill'
+            alt='Reddit Alien'
+          />
+        </div>
+        <p className='text-gray-400'>Sign in</p>
       </div>
     </div>
   )
